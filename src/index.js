7a70ec1486ec1a1efaba604787f1cd30d5a7aab5
Fedya Styopochkin <fst20071221@gmail.com>
2026-03-07 16:23:17 +0000

    Add hardcoded data for testing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './main.css'
+import { ProjectManager } from './modules/ProjectManager.js'
+import Project from './modules/Project.js';
+import Task from './modules/Task.js';
+
+function addSeedData(manager) {
+    const project = new Project('DNU')
+    const tasks = [
+        new Task("Лаба з C++", "Реалізувати паттерн Singleton", "2026-03-12", "High"),
+        new Task("Вивчити Git", "Розібратися з merge conflicts", "2026-03-10", "Medium"),
+        new Task("Переглянути лекцію", "Тема: Web Components", "2026-03-09", "Low")
+    ];
+
+    tasks.forEach(el => project.addTask(el))
+
+    manager.addProject(project)
+}
+
+addSeedData(ProjectManager)
+
+console.log(ProjectManager.getProjects())
