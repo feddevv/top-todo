e5373546c984e4f657b48df02b945021dc8f7dcd
Fedya Styopochkin <fst20071221@gmail.com>
2026-03-07 16:14:17 +0000

    Add Project class

---
+++ src/modules/Project.js
@@ -0,0 +1,27 @@
+export default class Project {
+    #tasks = []
+
+    constructor(name) {
+        this.id = crypto.randomUUID()
+        this.name = name
+    }
+
+    addTask(task) {
+        this.#tasks.push(task)
+    }
+
+    deleteTask(taskId) {
+        this.#tasks.forEach((el, index) => {
+            if (el.id === taskId) {
+                this.#tasks.splice(index, 1)
+                return true
+            }
+        })
+
+        return false
+    }
+
+    get tasks() {
+        return this.#tasks
+    }
+}
